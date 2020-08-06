a6efa74fdfa3f1d39fdc19d59fdf64abdfe3e864
Clementine <clementinenocton@hotmail.fr>
2020-08-06 21:20:39 +0200

    Iterations 1 - 7 done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = items;
+    this.length = items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, value) => acc += value);
+  }
 
-  avg() {}
+  avg() {
+    return this.sum() / this.length;
+  }
 }
+
+let list = new SortedList([2, 5, 6, 7, 8, 6, 9, 0]);
+console.log(list.length);
+console.log(list.avg());
 
 module.exports = SortedList;
